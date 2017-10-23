ff222bc386de4f82d0f655b2d97d57abe2b9cc31
sajana <sajana_b@hotmail.com>
2017-10-23 22:41:53 +0545

    Updating local repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 ## This function creates a "matrix" which can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -19,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
 ## This function computes the inverse of the "matrix" 
 ## returned by makeCacheMatrix above.If the inverse has 
 ## already been calculated and the matrix has not changed
